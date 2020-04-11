02a655f8914a4098cb94f270cef6e4abc847b9fe
Noah <noah.a.meltzer@gmail.com>
2020-04-10 23:25:19 -0500

    Use the dictionary viewer to display the heirarchy of windows

--- Test/window_test.py
+++ Test/window_test.py
@@ -1,7 +1,13 @@
 
 from Elements.MWindowManager import MWindowManager
+from Widgets.DictionaryViewer import DicionaryViewer
 
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QFontDialog, QFileDialog, QColorDialog
+
+def refresh_win_heirarchy():
+    win_hierarchy_viewer.fill_widget(window_manager.get_window_hierarchy())
+
+
 app = QApplication([])
 
 window_manager = MWindowManager()
@@ -10,6 +16,8 @@ color_dialog = QColorDialog()
 
 file_dialog = QFileDialog()
 
+win_hierarchy_viewer = DicionaryViewer()
+window_manager.window_hierarchy_updated_sig.connect(refresh_win_heirarchy)
 
 font_dialog = QFontDialog()
 
@@ -22,12 +30,14 @@ window_manager.add_window(label, "Hello world")
 window_manager.add_window(font_dialog, "Fonts")
 window_manager.add_window(file_dialog, "Files")
 window_manager.add_window(color_dialog, "Colors")
+window_manager.add_window(win_hierarchy_viewer, "Window Hierarchy")
 
 window_manager.show()
 
 font_dialog.show()
 file_dialog.show()
 label.show()
+
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
